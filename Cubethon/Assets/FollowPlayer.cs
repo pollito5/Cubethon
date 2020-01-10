385ffdf964b20629b475e19ebc79ce0dae09cfce
pollito5 <44715289+pollito5@users.noreply.github.com>
2020-01-10 00:21:06 -0700

    Uploaded from home pc
    Init commit with progress up until episode 5 of 9 part series. Cube will move and end when collision with obstacle is detected.

---
+++ Cubethon/Assets/FollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform m_Player;
+    public Vector3 m_CameraOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = m_Player.transform.position;
+        this.transform.position += m_CameraOffset;
+    }
+}
---
+++ Cubethon/Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody m_RB;
+    public float m_ForwardForce = 2000.0f, //use f to denote entry of a float value.
+                 m_MovementForce = 500.0f;
+
+    private bool m_MoveLeft = false,
+                 m_MoveRight = false;
+
+    //Update is called once per frame - faster than Fixed Update which is better for getting player inputs
+    void Update()
+    {
+        //Player inputs
+        if(Input.GetKey("d"))
+        {
+            m_MoveRight = true;
+        }
+        if(Input.GetKey("a"))
+        {
+            m_MoveLeft = true;
+        }
+    }
+
+    // Update is called once per frame - @@@FixedUpdate to be used for code interacting with physics.
+    void FixedUpdate()
+    {
+        //Add a force to the body
+        m_RB.AddForce(0, 0, m_ForwardForce * Time.deltaTime); //delta time is the number of seconds between frames to be used for portability.
+
+        if (m_MoveRight)
+        {
+            m_RB.AddForce(m_MovementForce, 0, 0);
+        }
+
+        if(m_MoveLeft)
+        {
+            m_RB.AddForce(-m_MovementForce, 0, 0); //flip direction
+        }
+
+        //R
+        m_MoveLeft = false;
+        m_MoveRight = false;
+    }
+}
